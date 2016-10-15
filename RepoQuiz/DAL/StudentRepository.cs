6750b1203cfd55996c8f8ee43b6f70c5653687a4
complikatyed <katye.russell@gmail.com>
2016-10-15 11:49:36 -0500

    Add repository file and inital context setup

---
+++ RepoQuiz/DAL/StudentRepository.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using RepoQuiz.Models;
+
+namespace RepoQuiz.DAL
+{
+    public class StudentRepository
+    {
+        public StudentContext Context { get; set; }
+
+        public StudentRepository()
+        {
+            Context = new StudentContext();
+        }
+
+        public StudentRepository(StudentContext _context)
+        {
+            Context = _context;
+        }
+    }
+}
