e832772ee5b3ad0101bf31925905ffcfe9be461b
complikatyed <katye.russell@gmail.com>
2016-10-17 20:26:46 -0500

    Set up GetStudents method to return ViewBag and View

--- RepoQuiz/Controllers/StudentController.cs
+++ RepoQuiz/Controllers/StudentController.cs
@@ -3,22 +3,24 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using RepoQuiz.DAL;
+using RepoQuiz.Models;
 
 namespace RepoQuiz.Controllers
 {
     public class StudentController : Controller
     {
+       
+        private StudentRepository repo = new StudentRepository();
+
         // GET: Student
         public ActionResult Index()
         {
+            List<Student> list_of_students = repo.GetStudents();
+            ViewBag.Students = list_of_students;
+
             return View();
         }
-
-        // GET: Student/Details/5
-        public ActionResult Details(int id)
-        {
-            return View();
-        }
-
+ 
     }
 }
