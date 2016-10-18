07790cfb1eb9d129c1e25e92dd5b8891b6631409
complikatyed <katye.russell@gmail.com>
2016-10-17 20:25:35 -0500

    Add randomizers for parts of student model - no idea how to test these

--- RepoQuiz/DAL/NameGenerator.cs
+++ RepoQuiz/DAL/NameGenerator.cs
@@ -2,13 +2,56 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Web;
+using RepoQuiz.Models;
 
 namespace RepoQuiz.DAL
 {
     public class NameGenerator
     {
-        // This class should be used to generate random names and Majors for Students.
-        // This is NOT your Repository
-        // All methods should be Unit Tested :)
+        List<string> firstnames = new List<string> { "Ahab", "Alejandro", "Alexandra", "Ahmad", "Anita", "Alyssa", "Andre", "Amit", "Anja", "Alice", "Alexa", "Alan" };
+        List<string> lastnames = new List<string> { "Bellisarius", "Bertram", "Bell", "Ballard", "Boone", "Beck", "Bellingham", "Ballustrade", "Beech", "Buckingham" };
+        List<string> majors = new List<string> { "Women's Studies", "Anthropology", "Economics", "Art History", "Biology", "Psychology", "Archeology", "Classics" };
+
+        public string ChooseRandomString(List<string> list)
+        { 
+            Random rnd = new Random();
+
+            int randomIndex = rnd.Next(list.Count);
+       
+            var chosenString = list[randomIndex];
+
+            return chosenString;
+        }
+
+        public string ChooseFirstName(List<string> firstnames)
+        {
+            string firstname = ChooseRandomString(firstnames);
+            return firstname;
+        }
+
+        public string ChooseLastName(List<string> lastnames)
+        {
+            string lastname = ChooseRandomString(lastnames);
+            return lastname;
+        }
+
+        public string ChooseMajor(List<string> majors)
+        {
+            string major = ChooseRandomString(majors);
+            return major;
+        }
+
+        public Student MakeRandomStudent()
+        {
+            string randomFirst = ChooseFirstName(firstnames);
+            string randomLast = ChooseLastName(lastnames);
+            string randomMajor = ChooseMajor(majors);
+
+            Student randomizedStudent = new Student { FirstName = randomFirst, LastName = randomLast, Major = randomMajor };
+                 
+            return randomizedStudent;
+        }
+
+
     }
 }
