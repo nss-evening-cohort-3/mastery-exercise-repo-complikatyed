5d35d40a0958bb9134e1b4ed86c0f70c91fdd495
complikatyed <katye.russell@gmail.com>
2016-10-17 20:43:32 -0500

    Begin setting up controller tests

--- RepoQuiz.Tests/Controllers/StudentControllerTests.cs
+++ RepoQuiz.Tests/Controllers/StudentControllerTests.cs
@@ -1,14 +1,34 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RepoQuiz.Controllers;
+using Moq;
+using RepoQuiz.DAL;
+using System.Web.Mvc;
+using RepoQuiz.Models;
+using System.Collections.Generic;
 
 namespace RepoQuiz.Tests.Controllers
 {
     [TestClass]
-    public class UnitTest1
+    public class StudentControllerTests
     {
-        [TestMethod]
-        public void TestMethod1()
+        private Mock<StudentRepository> mockStudentRepo;
+        private List<Student> students;
+
+        [TestInitialize]
+        public void TestIntialize()
         {
+            mockStudentRepo = new Mock<StudentRepository>();
+            students = new List<Student>();
+            mockStudentRepo.Setup(x => x.GetStudents()).Returns(students);
         }
+
+        [TestCleanup]
+        public void Cleanup()
+        {
+            mockStudentRepo = null;
+            students = null;
+        }
+
     }
 }
