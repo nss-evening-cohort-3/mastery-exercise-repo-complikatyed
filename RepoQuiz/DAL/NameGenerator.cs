452a348dfc82506dcee68ab11e3b6adad49b9f8d
complikatyed <katye.russell@gmail.com>
2016-10-19 17:51:32 -0500

    Namegenerator now working correctly

--- RepoQuiz/DAL/NameGenerator.cs
+++ RepoQuiz/DAL/NameGenerator.cs
@@ -13,37 +13,21 @@ namespace RepoQuiz.DAL
         List<string> lastnames = new List<string> { "Bellisarius", "Bertram", "Bell", "Ballard", "Boone", "Beck", "Bellingham", "Ballustrade", "Beech", "Buckingham" };
         List<string> majors = new List<string> { "Women's Studies", "Anthropology", "Economics", "Art History", "Biology", "Psychology", "Archeology", "Classics" };
 
+        Random rnd = new Random();
+
         public string ChooseRandomString(List<string> list)
         {
-            Random rnd = new Random();
             int randomIndex = rnd.Next(list.Count);
             var chosenString = list[randomIndex];
 
             return chosenString;
         }
-        public string ChooseFirstName(List<string> firstnames)
-        {
-            string firstname = ChooseRandomString(firstnames);
-            return firstname;
-        }
-
-        public string ChooseLastName(List<string> lastnames)
-        {
-            string lastname = ChooseRandomString(lastnames);
-            return lastname;
-        }
-
-        public string ChooseMajor(List<string> majors)
-        {
-            string major = ChooseRandomString(majors);
-            return major;
-        }
 
         public Student MakeRandomStudent()
         {
-            string randomFirst = ChooseFirstName(firstnames);
-            string randomLast = ChooseLastName(lastnames);
-            string randomMajor = ChooseMajor(majors);
+            string randomFirst = ChooseRandomString(firstnames);
+            string randomLast = ChooseRandomString(lastnames);
+            string randomMajor = ChooseRandomString(majors);
 
             Student randomizedStudent = new Student { FirstName = randomFirst, LastName = randomLast, Major = randomMajor };
 
